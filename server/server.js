354a4ba441bfab8fe1ee1aa605744ae7a8824623
Kyle <kyleludlow@gmail.com>
2017-01-26 20:00:53 -0800

    adds header, footer, index views. Adds index module, controller, routes. Adds index to client module dependencies. Updates index.html

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var path = require ('path')
 
 var db = require('./db.js')
 var middleware = require('./middleware')(db)
-
 
 var app = express()
 var PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   next()
+// })
+app.use(express.static(path.resolve(__dirname, '../client/')))
 
 app.get('/', function (req, res) {
   res.send('Root.')
